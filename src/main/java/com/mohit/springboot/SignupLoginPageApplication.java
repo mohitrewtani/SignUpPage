e78f1f9d44c8712ed93b5109695be10bddc0ffa2
MOHIT REWTANI <IN006190@adc.com>
2022-06-08 12:16:12 +0530

    Initital Spring Setup

---
+++ src/main/java/com/mohit/springboot/SignupLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupLoginPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/springboot/SignupLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupLoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
